281920c64756ac60f330465a3e5e7fb7cabc0fd5
Sarvesh Parab <sparab@usc.edu>
2019-01-19 11:07:58 -0800

    code to fetch upto a 1000 videos done query output also captured

--- hw1/youtube_query_template.py
+++ hw1/youtube_query_template.py
@@ -22,13 +22,13 @@ from googleapiclient.errors import HttpError
 #   https://cloud.google.com/console
 # Please ensure that you have enabled the YouTube Data API for your project.
 DEVELOPER_KEY = "AIzaSyBRCXsdLwzu082IjDxKDMUPO5BGt_FsSNM"
+# DEVELOPER_KEY = "AIzaSyCVrZhM9FgmwwVHB1SBPZacMHNnPeDmUi4"
 YOUTUBE_API_SERVICE_NAME = 'youtube'
 YOUTUBE_API_VERSION = 'v3'
 
 
-def youtube_search(query_term, max_results):
-    youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,
-        developerKey=DEVELOPER_KEY)
+def youtube_search(query_term, max_results, nextPageToken):
+    youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION, developerKey=DEVELOPER_KEY)
 
     # Call the search.list method to retrieve results matching the specified
     # query term.
@@ -37,7 +37,9 @@ def youtube_search(query_term, max_results):
         part='id,snippet',
         type='video',
         relevanceLanguage='en',
-        maxResults=max_results
+        maxResults=max_results,
+        order='viewCount',
+        pageToken=nextPageToken
     ).execute()
 
     video_ids = []
@@ -46,19 +48,54 @@ def youtube_search(query_term, max_results):
     # matching videos, channels, and playlists.
     for search_result in search_response.get('items', []):
         video_ids.append(search_result['id']['videoId'])
-    return video_ids
+
+    fetchedItems = len(search_response['items'])
+
+    nextPageTokenPresent = 'nextPageToken' in search_response
+
+    if nextPageTokenPresent and fetchedItems > 0:
+        nextPageToken = search_response['nextPageToken']
+    else:
+        nextPageToken = 'NEXT_Query'
+
+    return video_ids, nextPageToken
 
 
 if __name__ == '__main__':
     url_prefix = 'https://www.youtube.com/watch?v='
-    query_terms = '"recommender systems"'
-    max_results = 5
+    query_terms = ['"recommender systems"', '"information retrieval"', '"OLAP"', '"fuzzy logic"', '"Data querying"']
+
+    max_results = 50
+    netPages = 50
+    term_val = 0
+
+    nextPageToken = ''
+
+    net_ids = 0
 
     try:
-        ids = youtube_search(query_terms, max_results)
+        for pageCount in range(netPages):
+            print("Processing page : [ " + str(pageCount) + " ] with token : " + nextPageToken + " for query : [ " + query_terms[term_val] + " ]")
+            ids, nextPageToken = youtube_search(query_terms[term_val], max_results, nextPageToken)
+
+            if nextPageToken == 'NEXT_Query':
+                nextPageToken = ''
+                term_val += 1
+
+                if term_val < len(query_terms):
+                    continue
+                else:
+                    break
+
+            net_ids += len(ids)
+            print("Writing " + str(len(ids)) + " ids to file, making a total of : " + str(net_ids))
+
+            with open('query_output.txt', 'a') as f:
+                for i in ids:
+                    f.write(url_prefix + i + "\n")
+
+            if net_ids > 1000:
+                break
+
     except HttpError as e:
         print('An HTTP error %d occurred:\n%s' % (e.resp.status, e.content))
-    else:
-        with open('query_output.txt', 'w') as f:
-            for i in ids:
-                f.write(url_prefix+i+"\n")
